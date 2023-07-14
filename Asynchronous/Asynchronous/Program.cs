7e45163bbf853dcb78a48d16808115c4e882422e
Tursunboy <123323706+Tursunboy-005@users.noreply.github.com>
2023-07-14 15:59:27 +0500

    Task wait

--- Asynchronous/Asynchronous/Program.cs
+++ Asynchronous/Asynchronous/Program.cs
@@ -1,10 +1,91 @@
-﻿namespace Asynchronous
+﻿using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+namespace Asynchronous
 {
+    internal class Bacon { }
+    internal class Coffee { }
+    internal class Egg { }
+    internal class  Juice { }
+    internal class Toast { }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Coffee cup = PourCoffee();
+            Console.WriteLine("coffe is ready");
+
+            Egg eggs = FryEggs(2);
+            Console.WriteLine("eggs are ready");
+
+            Bacon bacon = FryBacon(3);
+            Console.WriteLine("bacon is ready");
+
+            Toast toast = ToastBread(2);
+            ApplyButter(toast);
+            ApplyJam(toast);
+            Console.WriteLine("toast is ready");
+
+            Juice juice 
+        }
+
+        private static Juice PourOJ()
+        {
+            Console.WriteLine("Pouring orange juice");
+            return new Juice();
+        }
+
+        private static void ApplyJam(Toast toast) => 
+            Console.WriteLine("Puttingjam on the toast");
+        private static void ApplyButter(Toast toast) =>
+            Console.WriteLine("Putting butter on the toast");
+        private static Toast ToastBread(int slices)
+        {
+            for(int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("Putting a slice of bread in the toaster");
+            }
+            Console.WriteLine("Start toasting...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Remove toast from toaster");
+
+            return new Toast();
+        }
+
+        private static Bacon FryBacon(int slices)
+        {
+            Console.WriteLine($"putting {slices} slices of bacon in the pan");
+            Console.WriteLine("cooking forst side of bacon...");
+            Task.Delay(3000).Wait();
+            for(int slice = 0; slice < slices; slice++)
+            {
+                Console.WriteLine("flipping a slice of bacon");
+            }
+            Console.WriteLine("cooking the second side of bacon");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put bacon on plate");
+
+            return new Bacon();
+        }
+
+        private static Egg FryEggs(int howMany)
+        {
+            Console.WriteLine("Warming the egg pan...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine($"cracking {howMany} eggs");
+            Console.WriteLine("cooking the eggs...");
+            Task.Delay(3000).Wait();
+            Console.WriteLine("Put eggs on plate");
+
+            return new Egg();
+        }
+
+        private static Coffee PourCoffee()
+        {
+            Console.WriteLine("  pouring coffee");
+            return new Coffee();
         }
     }
 }
